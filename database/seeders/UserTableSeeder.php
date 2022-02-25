359f4cfa85d1a46dbc7720387964a0c962616c8a
ah-mobin <ahmobin.cse@gmail.com>
2022-02-25 21:13:38 +0600

    ledger app init

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('Password500@'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
